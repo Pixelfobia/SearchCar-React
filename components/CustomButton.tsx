0d2054349faa015d152500c35f27f6fc93878f36
Sergio Utrera <sergiactive@gmail.com>
2023-09-29 11:23:20 +0100

    carga de datos ok

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 import { CustomButtonProps } from '@/types';
 
 const CustomButton = ({ title, containerStyles,
-	handleClick, btnType }: CustomButtonProps) => {
+	handleClick, btnType, textStyles, rightIcon }: CustomButtonProps) => {
 	return (
 		<button
 			disabled={false}
@@ -14,9 +14,19 @@ const CustomButton = ({ title, containerStyles,
 			onClick={handleClick}
 		>
 
-			<span className={`flex-1`}>
+			<span className={`flex-1 ${textStyles}`}>
 				{title}
 			</span>
+			{rightIcon && (
+				<div className="relative w-6 h-6">
+					<Image 
+						src={rightIcon}
+						alt="right icon"
+						fill
+						className="object-contain"
+					/>
+				</div>	
+			)}
 		</button>
 	)
 }
--- components/index.ts
+++ components/index.ts
@@ -2,10 +2,20 @@ import CustomButton from "./CustomButton";
 import Footer from "./Footer";
 import NavBar from "./Navbar";
 import Hero from "./Hero";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
+import SearchManufacturer from "./SearchManufacturer";
+import CarCard from "./CarCard";
+import CarDetails from "./CarDetails";
 
 export {
   Hero,
   CustomButton,
   Footer,
   NavBar,
+  SearchBar,
+  CustomFilter,
+  SearchManufacturer,
+  CarCard,
+  CarDetails
 };
